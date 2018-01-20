e9b701795c9db1dc60b07d8156684b6d72a0eb86
Kasvit <kasvit93@gmail.com>
2018-01-20 09:37:16 -0800

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence: true
 	validates :title, uniqueness: true
-	validates :description
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'URL має вказувати на формат картинки GIF, JPG або PNG'
